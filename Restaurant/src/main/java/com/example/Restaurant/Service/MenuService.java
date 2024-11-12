bbe6c1745bf37dd9ca20e96b33b2ed73b00e5f1f
naresh.mahadevan <nareshmaha43@gmail.com>
2024-11-12 12:44:14 +0530

    addupdatedelete

--- Restaurant/src/main/java/com/example/Restaurant/Service/MenuService.java
+++ Restaurant/src/main/java/com/example/Restaurant/Service/MenuService.java
@@ -1,4 +1,26 @@
 package com.example.Restaurant.Service;
 
+import com.example.Restaurant.Repositry.MenuRepository;
+import com.example.Restaurant.model.MenuItems;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class MenuService {
+    @Autowired
+    MenuRepository menuRepository;
+
+    public MenuItems addMenuItem(MenuItems menuItems){
+        return menuRepository.save(menuItems);
+
+    }
+    public MenuItems updateMenuItem(int id, MenuItems menuItems) {
+        if (menuRepository.existsById(id)) {
+            menuItems.setId(id);
+            return menuRepository.save(menuItems);
+        }
+        return null;
+    }
+
+    public void deleteMenuItem(int id) {
+        menuRepository.deleteById(id);
+    }
 }
