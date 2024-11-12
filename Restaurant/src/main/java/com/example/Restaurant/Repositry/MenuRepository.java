21f8ddd934cd3af6f79f668d38664e25559dffa2
naresh.mahadevan <nareshmaha43@gmail.com>
2024-11-12 13:19:22 +0530

    getall

--- Restaurant/src/main/java/com/example/Restaurant/Repositry/MenuRepository.java
+++ Restaurant/src/main/java/com/example/Restaurant/Repositry/MenuRepository.java
@@ -3,5 +3,5 @@ package com.example.Restaurant.Repositry;
 import com.example.Restaurant.model.MenuItems;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface MenuRepository extends MongoRepository<MenuItems, String> {
+public interface MenuRepository extends MongoRepository<MenuItems, Integer> {
 }
--- Restaurant/src/main/java/com/example/Restaurant/Service/MenuService.java
+++ Restaurant/src/main/java/com/example/Restaurant/Service/MenuService.java
@@ -1,4 +1,32 @@
 package com.example.Restaurant.Service;
 
+import com.example.Restaurant.Repositry.MenuRepository;
+import com.example.Restaurant.model.MenuItems;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class MenuService {
+    @Autowired
+    MenuRepository menuRepository;
+
+    public MenuItems addMenuItem(MenuItems menuItems){
+        return menuRepository.save(menuItems);
+
+    }
+    public MenuItems updateMenuItem(int id, MenuItems menuItems) {
+        if (menuRepository.existsById(id)) {
+            menuItems.setId(id);
+            return menuRepository.save(menuItems);
+        }
+        return null;
+    }
+
+    public void deleteMenuItem(int id) {
+        menuRepository.deleteById(id);
+    }
+
+    public List<MenuItems> getAllMenuItems() {
+        return menuRepository.findAll();
+    }
 }
